76cf30258545e729bc1a744336aa1212383d568c
Sachinxmpl <sachinxmpl6@gmail.com>
2024-10-06 07:41:24 +0545

    int

---
+++ apps/merchant-app/app/api/bot/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export function POST(){
+            return NextResponse.json({
+                        "name" : "sachin" , 
+                        "age" : 20 
+            })
+}
